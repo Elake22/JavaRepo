073d36fa98bc4a6637578b3e9e3fa5503676ced9
falcoriader <elijahlake2@gmail.com>
2025-05-28 10:29:37 -0500

    Update Main.java

--- ExerciseOperations/Levelup/src/Main.java
+++ ExerciseOperations/Levelup/src/Main.java
@@ -2,12 +2,32 @@
 
 public class Main {
     public static void main(String[] args) {
+        int currentXP = 1200; // experience points
+        int level = 5;
+        int xpToNextLevel = 1500;
+        boolean levelUp;
 
-        System.out.printf("Hello and welcome!");
+        // Quest complete +300XP
+        currentXP += 300; //1500
+        xpToNextLevel -= 300; // XP remaining
 
-        for (int i = 1; i <= 5; i++) {
+        // Double XP
+        currentXP *= 2;
 
-            System.out.println("i = " + i);
-        }
+        // Next level condition check
+        boolean xpFilled = currentXP >= xpToNextLevel;
+        boolean isMaxLevel = level >= 10;
+
+        // Conditions met
+        levelUp = xpFilled && !isMaxLevel;
+        boolean isPro = level > 7 || currentXP > 5000;
+
+        System.out.printf("Player Stats: ");
+        System.out.printf("Current XP: " + currentXP);
+        System.out.printf("  Level: " + level);
+        System.out.printf("  XP to next Level: " + xpToNextLevel);
+        System.out.printf("  Level UP!: " + levelUp);
+        System.out.printf("  Pro: " + isPro);
+
     }
 }
