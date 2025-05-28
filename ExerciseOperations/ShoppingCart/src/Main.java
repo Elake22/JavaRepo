dbc2cd875466829a1382f6a17e7dfa8aa05a3357
falcoriader <elijahlake2@gmail.com>
2025-05-28 09:38:12 -0500

    Shopping cart

--- ExerciseOperations/ShoppingCart/src/Main.java
+++ ExerciseOperations/ShoppingCart/src/Main.java
@@ -3,11 +3,27 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
+        System.out.printf("Shopping Cart");
 
-        for (int i = 1; i <= 5; i++) {
+        // 1. Product Info
+        int productID = 1;
+        int productCategory =2;
+        double productCost = 2.56;
+        double productPrice = 4.99;
+        int productQuantity = 78;
 
-            System.out.println("i = " + i);
-        }
+        // 2. Total cost based on Inventory
+        double totalCost = productCost * productQuantity;
+        System.out.println(" Total Cost: $" + totalCost );
+
+        // 3. Profit Margin Price - cost
+        double productProfit = productPrice - productCost;
+        System.out.println("Profit  Margin: $" + productProfit);
+
+        // 4. Profit Potential   product x Quantity
+        double totalProfit = productProfit * productQuantity;
+        System.out.println("Total Possible Profit: $" + totalProfit);
+
+
     }
 }
