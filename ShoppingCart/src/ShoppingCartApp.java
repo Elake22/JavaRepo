f7a8bac06dc8fd4daedf3afd09139cb5772266dd
falcoriader <elijahlake2@gmail.com>
2025-05-29 17:14:41 -0500

    Cart IO

--- ShoppingCart/src/ShoppingCartApp.java
+++ ShoppingCart/src/ShoppingCartApp.java
@@ -1,4 +1,5 @@
 // Shopping Cart
+import java.util.Scanner;
 
 public class ShoppingCartApp {
 
@@ -12,8 +13,9 @@ public class ShoppingCartApp {
     }
 
     public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
 
-        System.out.printf("Shopping Cart");
+        System.out.println("Shopping Cart");
 
         // 1. Product Info
         int productID = 1;
@@ -22,9 +24,10 @@ public class ShoppingCartApp {
         double productPrice = 4.99;
         int productQuantity = 78;
 
+        System.out.println("---------Cost and Profit--------");
         // 2. Total cost based on Inventory
         double totalCost = productCost * productQuantity;
-        System.out.println(" Total Cost: $" + totalCost );
+        System.out.println("Total Cost: $" + totalCost );
 
         // 3. Profit Margin Price - cost
         double productProfit = productPrice - productCost;
@@ -34,6 +37,7 @@ public class ShoppingCartApp {
         double totalProfit = productProfit * productQuantity;
         System.out.println("Total Possible Profit: $" + totalProfit);
 
+        System.out.println("---------Shipping & Status--------");
         System.out.println(ShippingStatus.STANDARD);
         System.out.println(ShippingStatus.TWO_DAY);
         System.out.println(ShippingStatus.OVERNIGHT);
@@ -48,9 +52,36 @@ public class ShoppingCartApp {
         String businessContactInfo = "123-456-7890";
         String productDescription = "My product description";
 
+        System.out.println("---------Business--------");
         System.out.println("Business name: " + businessName);
         System.out.println("Business contact info: " + businessContactInfo);
         System.out.println("Product description: " + productDescription);
+
+
+        //------Console I/O Section_______
+        // Prompt for tax exempt
+        System.out.println("---------Tax Exemption, Shipping, Promo Code--------");
+        System.out.println("Are you tax-exempt? (y/n)");
+        String taxExempt = console.nextLine();
+
+        // Prompt for shipping
+        System.out.print("Shipping? (y/n) ");
+        String shipping = console.nextLine();
+
+        // Prompt for order quantity
+        System.out.print("Order quantity? ");
+        int orderQuantity = Integer.parseInt(console.nextLine());
+
+        // Prompt for promo code
+        System.out.print("Promo code for free shipping? ");
+        String promoCode = console.nextLine();
+
+        // IO prints
+        System.out.println("\nDetails:");
+        System.out.println("Tax-exempt: " + taxExempt);
+        System.out.println("Shipping: " + shipping);
+        System.out.println("Order quantity: " + orderQuantity);
+        System.out.println("Promo code: " + promoCode);
 
     }
 }
