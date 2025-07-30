b6db1e1bdf385350265bbfffd7f096753d69fc94
falcoriader <elijahlake2@gmail.com>
2025-07-30 09:32:17 -0500

    cota

--- SQL/SQL Summative/databases-in-java/summative-assessment-java-databases-starter/src/main/java/org/example/data/impl/OrderRepoImpl.java
+++ SQL/SQL Summative/databases-in-java/summative-assessment-java-databases-starter/src/main/java/org/example/data/impl/OrderRepoImpl.java
@@ -14,6 +14,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import org.springframework.jdbc.core.CallableStatementCallback;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.transaction.annotation.Transactional;
 
 
 import java.sql.*;
@@ -174,6 +175,7 @@ public class OrderRepoImpl implements OrderRepo {
     }
 
     @Override
+    @Transactional
     public Order deleteOrder(int id) throws InternalErrorException {
         try {
             Order toDelete = getOrderById(id);
